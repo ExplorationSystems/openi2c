1157d5427f1621fe8f5eb28af3a0421e5a28cf63
Kyle <kyle.ueckermann@gmail.com>
2023-12-02 01:40:38 +0100

    PCA9685 done

--- src/PCA9685/test.ts
+++
@@ -1,9 +0,0 @@
-import { PCA9685, MODE1 } from './index';
-async function main() {
-    const pwmDriver = new PCA9685();
-    console.log('setPWMFreq', await pwmDriver.readByte(MODE1))
-    // await pwmDriver.setPWMFreq(50);
-    await pwmDriver.setPWM(0, 0, 2000); // Example usage
-}
-
-main();
