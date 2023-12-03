953ee91cee1ee0c3c0dc1b082d94b960b20d3b27
Kyle <kyle.ueckermann@gmail.com>
2023-12-03 01:31:53 +0100

    build

--- dist/modules/PCA9685/index.js
+++ dist/modules/PCA9685/index.js
@@ -35,6 +35,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PCA9685 = exports.PRESCALE = exports.MODE1 = exports.PCA9685_ADDRESS = void 0;
 const i2c = __importStar(require("i2c-bus"));
 const utils_1 = require("../../utils");
+const debug_1 = require("../../debug");
 exports.PCA9685_ADDRESS = 0x40;
 exports.MODE1 = 0x00;
 exports.PRESCALE = 0xfe;
@@ -42,6 +43,7 @@ const LED0_ON_L = 0x06;
 const LED0_ON_H = 0x07;
 const LED0_OFF_L = 0x08;
 const LED0_OFF_H = 0x09;
+const log = debug_1.debug.extend('PCA9685');
 class PCA9685 {
     constructor(busNumber = 0, address = exports.PCA9685_ADDRESS) {
         this.bus = i2c.openSync(busNumber).promisifiedBus();
@@ -60,13 +62,13 @@ class PCA9685 {
     setDutyCycle(channel, dutyCycle) {
         return __awaiter(this, void 0, void 0, function* () {
             dutyCycle = Math.min(1, Math.max(0, dutyCycle)); // Clamp to [0,1]
-            console.log(`Set duty cycle for channel ${channel} to ${dutyCycle}`);
+            log(`Set duty cycle for channel ${channel} to ${dutyCycle}`);
             yield this.setPWM(channel, 0, Math.round(dutyCycle * 4095));
         });
     }
     setFrequency(freq) {
         return __awaiter(this, void 0, void 0, function* () {
-            console.log(`Set PWM frequency to ${freq} Hz`);
+            log(`Set PWM frequency to ${freq} Hz`);
             const prescale = Math.round(25000000 / (4096 * freq)) - 1;
             yield this.writeByte(exports.MODE1, 0x10); // sleep
             yield this.writeByte(exports.PRESCALE, prescale); // set frequency prescaler
@@ -78,7 +80,7 @@ class PCA9685 {
     }
     setPWM(channel, on, off) {
         return __awaiter(this, void 0, void 0, function* () {
-            console.log(`Set PWM for channel ${channel} to ${on} to ${off}`);
+            log(`Set PWM for channel ${channel} to ${on} to ${off}`);
             yield Promise.all([
                 this.bus.writeByte(this.address, LED0_ON_L + 4 * channel, on & 0xff),
                 this.bus.writeByte(this.address, LED0_ON_H + 4 * channel, on >> 8),
